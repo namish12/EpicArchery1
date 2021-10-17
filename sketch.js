17c2e6394b3cbce9de8fe3aad334562dffc9e412
namish12 <89356264+namish12@users.noreply.github.com>
2021-10-17 11:45:47 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,36 +5,19 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var canvas;
-var player, playerBase;
+var palyer, playerBase;
 var computer, computerBase;
 
-/*function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
-}*/
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
 
   engine = Engine.create();
   world = engine.world;
-  //angleMode(DEGREES);
 
-  var options = {
-    isStatic : true
-  };
-
-  //create player base body
-  //playerBase = Bodies.rectangle(200, 350, 180, 150, options);
-  //World.add(world, playerBase);
-  //create player body
-  //player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
-  //World.add(world, player);
-  //create player archer
- 
   playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
   player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+
 
   computerBase = new ComputerBase(
     width - 300,
@@ -49,17 +32,11 @@ function setup() {
     180
   );
 
+  
 }
 
 function draw() {
   background(189);
-
-  //show the player image using image() function
-  //image(baseimage, playerBase.position.x, playerBase.position.y, 180, 150);
-  //show the playerbase image using image() function
-  //image(playerimage, player.position.x, player.position.y, 50, 180);
-  //displays the Computer
-  
 
   Engine.update(engine);
 
@@ -69,10 +46,13 @@ function draw() {
   textSize(40);
   text("EPIC ARCHERY", width / 2, 100);
 
+ 
   playerBase.display();
   player.display();
   
 
   computerBase.display();
   computer.display();
+  
+
 }
